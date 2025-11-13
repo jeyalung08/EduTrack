5b87807e992632caa6829091b5fb0ddf39b8189c
Emman Yalung <Emman Yalung@LAPTOP-RIH0M3RT>
2025-11-13 23:01:55 +0800

    Update 2.4

--- app/src/main/java/ph/edu/auf/emman/yalung/edutrack/viewmodel/StudentViewModel.kt
+++ app/src/main/java/ph/edu/auf/emman/yalung/edutrack/viewmodel/StudentViewModel.kt
@@ -1,4 +1,20 @@
 package ph.edu.auf.emman.yalung.edutrack.viewmodel
 
-class StudentViewModel {
+
+import androidx.lifecycle.ViewModel
+import ph.edu.auf.emman.yalung.edutrack.repository.StudentRepository
+
+
+class StudentViewModel(
+    private val studentRepository: StudentRepository
+) : ViewModel() {
+
+    // StateFlow to hold the list of students
+
+    fun loadStudents(classId: String) {
+        // viewModelScope.launch {
+        //   _students.value = studentRepository.getStudentsForClass(classId)
+        // }
+    }
 }
+
