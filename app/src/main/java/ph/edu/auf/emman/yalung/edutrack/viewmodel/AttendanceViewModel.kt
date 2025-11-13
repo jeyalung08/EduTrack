57649a59a51b6175bc58853f95fe4f98e585116c
Marlon-Arch <bondoc.marlonjan@auf.edu.ph>
2025-11-13 23:15:03 +0800

    Update 2.5

--- app/src/main/java/ph/edu/auf/emman/yalung/edutrack/viewmodel/AttendanceViewModel.kt
+++ app/src/main/java/ph/edu/auf/emman/yalung/edutrack/viewmodel/AttendanceViewModel.kt
@@ -1,4 +1,27 @@
 package ph.edu.auf.emman.yalung.edutrack.viewmodel
 
-class AttendanceViewModel {
+
+import androidx.lifecycle.ViewModel
+import ph.edu.auf.emman.yalung.edutrack.repository.AttendanceRepository
+
+
+class AttendanceViewModel(
+    private val attendanceRepository: AttendanceRepository
+) : ViewModel() {
+
+    fun markAsPresent(studentId: String) {
+        // ...
+    }
+
+    fun markAsLate(studentId: String) {
+        // ...
+    }
+
+    fun submitAttendance() {
+        // viewModelScope.launch {
+        //   val records = ... create List<Attendance> from state
+        //   attendanceRepository.submitAttendanceBatch(records)
+        // }
+    }
 }
+
